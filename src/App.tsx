f7fea1f53c99355535c681864d0e8d2cde40af7d
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-07-26 08:40:36 +0200

    inifinite loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import { useEffect, useRef } from "react";
-
+import { ProductList } from "./ProductList";
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (ref.current) ref.current.focus();
-  });
-  useEffect(() => {
-    document.title = "My React";
-  });
   return (
     <>
-      <div>
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ProductList />
     </>
   );
 }
---
+++ src/ProductList.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function ProductList() {
+  const [products, setProducts] = useState<string>([]);
+
+  useEffect(() => {
+    console.log("Fetching products");
+    setProducts(["clothing", "electronics"]);
+  });
+
+  return (
+    <>
+      <div>ProductList</div>
+    </>
+  );
+}
+
+export default ProductList;
